97a8549934d02bf870eb9b182e4b0c0f86487c01
Alejxdp <demeralejandro13@gmail.com>
2025-06-04 08:40:55 +0000

    sd

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const boton = document.getElementById('materias-btn');
+  const mostrarBtn = document.getElementById('materias-btn');
+  const volverBtn = document.getElementById('volver-btn');
   const infoGeneral = document.getElementById('info-general');
   const materiasContainer = document.getElementById('materias-container');
   const infografiaBtns = document.querySelectorAll('.info-btn');
+  const seccionBoton = document.querySelector('.materias-section');
 
-  boton.addEventListener('click', () => {
+  mostrarBtn.addEventListener('click', () => {
     infoGeneral.style.display = 'none';
-    boton.style.display = 'none';
+    seccionBoton.style.display = 'none';
     materiasContainer.classList.remove('hidden');
+  });
+
+  volverBtn.addEventListener('click', () => {
+    infoGeneral.style.display = 'flex';
+    seccionBoton.style.display = 'block';
+    materiasContainer.classList.add('hidden');
+
+    // Oculta cualquier infografía abierta
+    document.querySelectorAll('.extra-info').forEach(info => info.classList.add('hidden'));
   });
 
   infografiaBtns.forEach((btn) => {
